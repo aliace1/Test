df8038079f9639709f4bcfef28698346f3556a8d
Aliace Didier <rasolonjatovomartino@gmail.com>
2021-07-20 06:23:08 +0300

    Message

--- react-coding-challenges/chatter/src/components/Messages/components/Messages.js
+++ react-coding-challenges/chatter/src/components/Messages/components/Messages.js
@@ -8,6 +8,8 @@ import Header from './Header';
 import Footer from './Footer';
 import Message from './Message';
 import '../styles/_messages.scss';
+import { sendMessage, isTyping } from 'react-chat-engine';
+import { props } from 'bluebird';
 
 const socket = io(
   config.BOT_SERVER_ENDPOINT,
@@ -20,12 +22,18 @@ function Messages() {
   const { setLatestMessage } = useContext(LatestMessagesContext);
 
   const [value, setValue] = useState('');
+  const { chatId, creds } = props;
+
   const onChangeMessage = (event) => {
     setValue(event.target.value);
+
+    isTyping(props, chatId)
   };
 
   const handleSubmit = (event) => {
     // event.preventDefault();
+    const text = value.trim();
+    if (text.length > 0) sendMessage(creds, chatId, { text })
 
     console.log('*****Valeur*******', value);
   };
